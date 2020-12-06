a6bb3ee6375781b2a6c0de072b61d321414bd548
Mohamed Adam <64768782+Mohamed-Adm@users.noreply.github.com>
2020-12-06 13:36:51 +0200

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,17 +1,25 @@
 window.onload = function () {
+  document.getElementById("rebc").onclick = Rebecca;
+  document.getElementById("yellow").onclick = yellow;
   document.getElementById("red").onclick = Red;
-  document.getElementById("blue").onclick = Blue;
   document.getElementById("green").onclick = Green;
 };
+function Green(){
+	var green = document.getElementById("outer-block");
+  green.style.backgroundColor = "green";
+  green.style.border = '5px solid #f0f2f5';
+  green.style.borderRadius ='.9rem';
+}
 function Red() {
   var red = document.getElementById("outer-block");
-  red.style.backgroundColor = "brown";
+  red.style.backgroundColor = "red";
 }
-function Blue() {
-  var blue = document.getElementById("outer-block");
-  blue.style.backgroundColor = "blue";
+function yellow() {
+  var yellow = document.getElementById("outer-block");
+  yellow.style.backgroundColor = "yellow";
 }
-function Green() {
-  var grey = document.getElementById("outer-block");
-  grey.style.backgroundColor = "green";
+function Rebecca() {
+  var rebecca = document.getElementById("outer-block");
+  rebecca.style.backgroundColor = "#3366ff";
 }
+
